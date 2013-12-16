c7236c008a49c57b75b6fa2fb5820b336cfb8e3f
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2013-12-17 00:31:59 +0100

    Large refactor, no more many-to-many relations, actually working javascript.

--- service/staff.py
+++ service/staff.py
@@ -6,6 +6,9 @@ import simplejson as json
 from flask import Flask, jsonify, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
+tags = ['Scientific staff', 'Tech+Adm staff', 'PhD students', 'Students', 'Guests', 'Former members']
+groups = ['Ice and Climate', 'Meteorology', 'Oceanography']
+
 # url_for('static', filename='style.css')
 
 app = Flask(__name__)
@@ -13,36 +16,30 @@ app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/runejuhl/projects/staff/service/staff.sqlite3'
 db = SQLAlchemy(app)
 
-groups = db.Table(
-    'groups',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
-    db.Column('person_id', db.Integer, db.ForeignKey('person.id'))
-)
-
-tags = db.Table(
-    'tags',
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
-    db.Column('person_id', db.Integer, db.ForeignKey('person.id'))
-)
+@app.template_filter('split')
+def reverse_filter(s):
+    return map(lambda(x): x.strip(), s.split(','))
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String)
     image_src = db.Column(db.String)
+    kuid = db.Column(db.Integer)
     room = db.Column(db.String)
     phone = db.Column(db.String)
-    tags = db.relationship('Tag', secondary=tags,
-                           backref=db.backref('persons', lazy='dynamic'))
-    groups = db.relationship('Group', secondary=groups,
-                             backref=db.backref('persons', lazy='dynamic'))
+    tags = db.Column(db.String)
+    groups = db.Column(db.String)
 
-    def __init__(self, name, email, image_src, room, phone):
+    def __init__(self, name, email, image_src, kuid, room, phone, tags, groups):
         self.name = name
         self.email = email
         self.image_src = image_src
+        self.kuid = kuid
         self.room = room
         self.phone = phone
+        self.tags = tags
+        self.groups = groups
 
     def __repr__(self):
         return json.dumps({
@@ -50,23 +47,12 @@ class Person(db.Model):
             'name'              : self.name,
             'email'             : self.email,
             'image_src'         : self.image_src,
+            'kuid'              : self.kuid,
             'room'              : self.room,
             'phone'             : self.phone,
+            'tags'              : self.tags,
+            'groups'            : self.groups,
         })
-
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-
-    def __init__(self, name):
-        self.name = name
-
-class Group(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-
-    def __init__(self, name):
-        self.name = name
 
 @app.route('/')
 def index():
@@ -76,14 +62,7 @@ def index():
 @app.route('/get/all')
 def get_all():
     # return jsonify(all = [i.serialize for i in Person.query.all()])
-    return render_template('businesscard.jinja2', persons = Person.query.all())
-
-@app.route('/raw/all')
-def raw_all():
-    all = Person.query.join(Group).all()
-    print all
-    return jsonify(all)
-    # return render_template('businesscard.jinja2', persons = Person.query.all())
+    return render_template('businesscard.jinja2', persons = Person.query.order_by(Person.name).all())
 
 @app.route('/get/<id>')
 def get_single(id):
@@ -98,14 +77,20 @@ if __name__ == '__main__':
         db.drop_all()
         db.create_all()
 
-        db.session.add(Person('Rune Juhl Jacobsen', 'runejuhl@nbi.ku.dk', None, 'RF-346', "6016 8337"))
-        db.session.add(Person('Thomas Blunier', 'blunier@nbi.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=337115', 'RF-201', None))
-
-        for tag in ['Scientific stafff', 'Tech+Adm staff', 'PhD students', 'Students', 'Guests', 'Former members']:
-            db.session.add(Tag(tag))
-        for group in ['Ice and Climate', 'Meteorology', 'Oceanography']:
-            db.session.add(Group(group))
-
+        db.session.add(Person('Rune Juhl Jacobsen', 'runejuhl@nbi.ku.dk', None, 410316, 'RF-346', "+45 6016 8337", 'Student', 'Ice and Climate'))
+        db.session.add(Person('Ellen Emme Chrillesen', 'ec@nbi.ku.dk', None, 8645, 'RF-229', '+45 353-20551', 'Tech+Adm staff', 'Ice and Climate'))
+        db.session.add(Person('Thomas Blunier', 'blunier@nbi.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=337115', 337115, 'RF-201', "+45 353-20584", 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Susanne Lilja Buchardt', 'lilja@gfy.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=181090', 181090, 'RF-201', "+45 353-20584", 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Viorela Gabriela Ciobanu', 'ciobanu@nbi.dk', None, 441353, 'RF-206', ' +45 353-20627', 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Ivana Cvijanovic', 'ivanacv@nbi.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=353050', 353050, None, '+45 28 40 57 82', 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Dorthe Dahl-Jensen', 'ddj@gfy.ku.dk', None, 45103, 'RF-316', '+45 353-20556', 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Peter Ditlevsen', 'pditlev@nbi.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=6225', 6225, 'RF-304', '+45 353-20603', 'Scientific staff', 'Ice and Climate'))
+        db.session.add(Person('Anne-Katrine Faber', 'akfaber@nbi.ku.dk', 'https://www2.adm.ku.dk/selv/pls/prt_www40.hentindhold_cms?p_personid=304875', 304875, 'RF-342', None, 'Student', 'Ice and Climate'))
+        # db.session.add(Person('', '', , '', '', '', '', 'Ice and Climate'))
+        # db.session.add(Person('', '', , '', '', '', '', 'Ice and Climate'))
+        # db.session.add(Person('', '', , '', '', '', '', 'Ice and Climate'))
+        # db.session.add(Person('', '', , '', '', '', '', 'Ice and Climate'))
         db.session.commit()
 
     app.run()
+
--- staff.js
+++ staff.js
@@ -1,86 +1,39 @@
+var staff = $('#staff');
+var bcards = $('.business-card')
 
-
-var staff = $.('#staff');
-
-
-// ---------------------------------------------
-
-// Convert an object to an array
-function toArray(c) {
-    return [].slice.call(c, 0);
-}
-
-var staff = document.getElementById('staff');
-var section = staff;
-var bcards = document.getElementsByClassName('business-card');
-
-function updateSelection() {
-    selection = toArray(bcards);
-
-    selection.map(function(x) {
-        x.classList.remove('hidden');
-    });
-
-    var filters = document.querySelectorAll('div.filter ul li.toggled');
-    filters = toArray(filters).map(function(x) { return x.innerHTML; });
-
-    for (var i = 0; i < filters.length; i++) {
-        for (var j = 0; j < selection.length; j++) {
-            var tags = selection[j].querySelectorAll('.tags li');
-            tags = toArray(tags);
-            // get tag values
-            tags = tags.map(function(y) { return y.innerHTML; });
-            
-            if (tags.indexOf(filters[i]) == -1) {
-                selection[j].classList.add('hidden');
-            }
-        }
-    }
-   
-}
-
-// Find out where to place search box
-while (section.nodeName != 'H1')
-    section = section.previousSibling;
-
-// Get all tags
-var tagelems = [].slice.call(document.getElementsByClassName('tags'), 0);
-var tagelems = tagelems.map(function(elem) { return [].slice.call(elem.children, 0).map(function(child) { return child.innerHTML;}) }).join();
-
-tagelems = tagelems.split(',');
-
-// Remove duplicates
 var tags = [];
-for (var i in tagelems) {
-    var e = tagelems[i];
-    if (tags[e] == undefined)
-        tags[e] = [e, 1];
-    else
-        tags[e][1] += 1;
-}
+var tmptags = staff.find('.tags li').map(function() { return this.textContent; }).sort();
 
-var searchbox = document.createElement('div');
-var searchul = document.createElement('ul');
-searchbox.className = 'filter';
-searchul.className = 'tags';
-searchbox.appendChild(searchul);
+for (var i = 0; i < tmptags.length; i++)
+    if (tags && tags[tags.length-1] != tmptags[i])
+        tags.push(tmptags[i])
+
+// Create the tag selection box
+var searchbox = $('<div class="filter"><ul class="tags"></ul></div>');
+var searchul = searchbox.find('ul');
+
 // Add tags to search box
-for (var i in tags) {
-    var e = tags[i];
-    var d = document.createElement('li');
-    d.innerHTML = e[0];
-    d.onclick = function(x) {
-        this.classList.toggle('toggled');
-        updateSelection();
-    }
-    searchul.appendChild(d);
-}
+$.map(tags, function(x) {
+    searchul.append('<li class="toggled">' + x + '</li>');
+});
 
 // Append searchbox
-section.appendChild(searchbox);
+staff.prepend(searchbox);
 
-// We should sort the list here...
-// console.log(index);
-// console.log(index.sort(function(a,b) { console.log("asdsad" + a + ": " + b); return a[1] > b[1];}));
+$('.filter .tags li').click(function() {
+    var curr = $(this).text();
+    // toggle clicked item
+    $(this).toggleClass('toggled');
 
-// console.log(index)
+    bcards.find('.tags').each(function() {
+        $(this).find('li').each(function() {
+            console.log($(this).text());
+            if ($(this).text() == curr) {
+                console.log($(this));
+                console.log($(this).parentsUntil('li'));
+                $(this).parents('li').toggle('toggled');
+                return false;
+            }
+        });
+    });
+});
