ca390ffddd490404e7fe95f5fcd6ff8fcb675bca
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2013-12-17 09:06:43 +0100

    Minor fixes, documentation

--- staff.js
+++ staff.js
@@ -1,8 +1,16 @@
+// containing div
 var staff = $('#staff');
+// all "business cards" have this class
 var bcards = $('.business-card')
 
+// generate a list of tags used in the document, sort them and remove
+// duplicates
 var tags = [];
-var tmptags = staff.find('.tags li').map(function() { return this.textContent; }).sort();
+var tmptags = staff.find('.tags li')
+    .map(function() {
+        return this.textContent;
+    })
+    .sort();
 
 for (var i = 0; i < tmptags.length; i++)
     if (tags && tags[tags.length-1] != tmptags[i])
@@ -20,6 +28,7 @@ $.map(tags, function(x) {
 // Append searchbox
 staff.prepend(searchbox);
 
+// Add function on click
 $('.filter .tags li').click(function() {
     var curr = $(this).text();
     // toggle clicked item
